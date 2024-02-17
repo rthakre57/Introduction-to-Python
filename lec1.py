51ed2e043391d7f03196bf97c7355ae066756a4a
ROSHAN THAKRE <135240184+rthakre57@users.noreply.github.com>
2024-02-17 13:45:36 +0530

    Create lec1.py

---
+++ lec1.py
@@ -0,0 +1,41 @@
+print("Hello World!")
+print('Hello World!')
+print(6834 + 57)
+print(6834 * 57)
+print(int(6834 / 57))
+print(6834 // 57) # Returns quotient
+print(6834 % 57) # Returns the remainder
+
+a = 6834
+b = 57
+print(a,b)
+q = a//b
+r = a%b
+print(q,r)
+print(b*q+r == a)
+print(27**5)
+print(27**50)
+#print(27**500)
+#print(27**10000)
+#print(27**100000)
+print(27**(0.5))
+
+a=2
+b=2
+print(a==b)
+
+Output:
+Hello World!
+Hello World!
+6891
+389538
+119
+119
+51
+6834 57
+119 51
+True
+14348907
+369988485035126972924700782451696644186473100389722973815184405301748249
+5.196152422706632
+True
