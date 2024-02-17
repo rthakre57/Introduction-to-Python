b948cf833dc8906bd198198c43f5985648b8c51f
ROSHAN THAKRE <135240184+rthakre57@users.noreply.github.com>
2024-02-17 13:54:24 +0530

    Create Lec3.py

---
+++ Lec3.py
@@ -0,0 +1,25 @@
+import math
+
+print(math.sqrt(2))
+
+print(math.sin(2.5))
+
+print(math.asin(-0.75))
+
+print(math.gcd(34352, 88262))
+
+help(math.log10)
+
+print(dir(math)) # To list all the functions/methods inside math modulecd deskcd
+
+Output:
+1.4142135623730951
+0.5984721441039564
+-0.848062078981481
+2
+Help on built-in function log10 in module math:
+
+log10(x, /)
+    Return the base 10 logarithm of x.
+
+['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
