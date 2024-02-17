983a50bf56d9f4de50348d7365baec86915ace27
ROSHAN THAKRE <135240184+rthakre57@users.noreply.github.com>
2024-02-17 18:30:56 +0530

    Create Lec22.py

---
+++ Lec22.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.normal(10,5,200)
+y = np.random.normal(10,5,200)
+plt.scatter(x,y)
+plt.show()
+
+x = np.random.normal(10,5,200)
+y = np.random.normal(10,5,200)
+plt.scatter(x,y,c='r',s=20)
+plt.title("Scatter plot")
+plt.xlabel('$x$-axis')
+plt.ylabel('$y$-axis')
+plt.show()
+
+Figure_1
+Figure_2
